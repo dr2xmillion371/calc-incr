9cfb04afcd68b6fc698597f8a710f4f85ad9e34b
dr2xmillion371 <119767559+dr2xmillion371@users.noreply.github.com>
2023-06-05 20:59:59 -0500

    Update photon_disco.py

--- mods/photon_disco.py
+++ mods/photon_disco.py
@@ -4,7 +4,7 @@ import time
 
 
 def photonDisco():
-  photons = ['This will last for 567 hours!!"]
+  photons = ['This will last for 567 hours!!']
   for i in photons:
              print(photons)
              time.sleep(2041200)
